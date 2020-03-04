bade60bc00c9de3fd8f336f5f4d952575e37b30f
Duncan Uruchima <duruchima1@gmail.com>
2020-03-04 20:45:54 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,35 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pidgeon_list = {}
+  data.each do |color_gender_lives, values|
+    values.each do |inner_value, array|
+    array.each do |pidgeon_name|
+      if pidgeon_list[pidgeon_name] == nil
+        pidgeon_list[pidgeon_name] = {}
+        pidgeon_list[pidgeon_name][color_gender_lives] = []
+      else
+        pidgeon_list[pidgeon_name][color_gender_lives] = []
+      end
+
+    end
+    end
+
+  end
+
+pidgeon_list.each do |pidgeon_name, values|
+  values.each do |pidgeon_list_key, array|
+    data.each do |color_gender_lives, values|
+      values.each do |inner_value, array|
+          array.each do |element|
+              if element == pidgeon_name && pidgeon_list_key == color_gender_lives
+                pidgeon_list[pidgeon_name][pidgeon_list_key] << inner_value.to_s
+          end
+      end
+
+    end
+
+  end
+  end
+end
+  pidgeon_list
 end
